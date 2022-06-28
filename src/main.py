5974a2e50ee2bc91c3aa8142936fc83821fbd9d9
BATU1579 <batu1234567890@outlook.com>
2022-06-29 04:32:51 +0800

    ✨ feat: 添加登陆后显示公司信息

--- src/main.py
+++ src/main.py
@@ -4,11 +4,21 @@
 Author: BATU1579
 CreateDate: 2022-06-28 15:28:17
 LastEditor: BATU1579
-LastTime: 2022-06-29 02:51:32
+LastTime: 2022-06-29 04:32:00
 FilePath: \\src\\main.py
 Description:
 '''
 from module.user_control import UserController
+from util.json_operation import load_file
+from util.input import Confirm
 
+# 登录
 controller = UserController()
 controller.loop()
+
+# 显示信息
+info = []
+for key, value in load_file(".\\src\\database\\company.json").items():
+    info.append(f"{key} - {value}")
+confirm = Confirm("Company Info", '\n     '.join(info), cancel_action=exit)
+confirm.get_input()
